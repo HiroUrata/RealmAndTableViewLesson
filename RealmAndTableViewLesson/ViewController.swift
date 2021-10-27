114be3e7350eaddbe248bdb63666efb5ce5d4dcb
HiroUrata <1712026@i.softbank.jp>
2021-10-27 16:17:56 +0900

    Initial Commit

---
+++ RealmAndTableViewLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmAndTableViewLesson
+//
+//  Created by UrataHiroki on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
